9784012c764415b3b77f024531c3714d20bcc7ec
Anderson Miranda <amiranda@dminc.com>
2024-04-23 01:02:50 +0200

    display cart count conditionally

--- header-ssr/src/components/CartIcon.tsx
+++ header-ssr/src/components/CartIcon.tsx
@@ -9,9 +9,11 @@ const CartIcon = () => {
   return (
     <div className="hd-flex hd-items-center hd-relative hd-mr-3">
       <Icon name="shopping-cart" fill="white" width={32} height={32} />
-      <div className="hd-bg-error-500 hd-p-1 hd-h-6 hd-w-6 hd-text-caption hd-rounded-full hd-justify-center hd-flex hd-absolute hd--top-2 hd-left-5">
-        {items[0].quantity}
-      </div>
+      {items[0].quantity > 0 && (
+        <div className="hd-bg-error-500 hd-p-1 hd-h-6 hd-w-6 hd-text-caption hd-rounded-full hd-justify-center hd-flex hd-absolute hd--top-2 hd-left-5">
+          {items[0].quantity}
+        </div>
+      )}
     </div>
   );
 };
